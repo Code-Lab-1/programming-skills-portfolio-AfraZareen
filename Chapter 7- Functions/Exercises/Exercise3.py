b7ec06013094682dbbe30bbc1f103c9195d47f38
Afra Zareen <afrazareen04@gmail.com>
2022-10-21 16:40:56 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,6 @@
+def make_shirts(size,message):
+    """Summarize the shirt that is going to be made."""
+    print(f"\nI am going to make a {size} shirt ")
+    print(f"The message is, '{message}'.")
+make_shirts("medium" , "Love Yourself")
+make_shirts("large" , "Believe in yourself")

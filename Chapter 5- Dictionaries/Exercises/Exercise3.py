9acf0cae7a24a6131d642c6cadc8aff4e7d19a51
Afra Zareen <afrazareen04@gmail.com>
2022-10-26 10:54:04 +0400

    Update Exercise3.py

--- Chapter 5- Dictionaries/Exercises/Exercise3.py
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -2,12 +2,12 @@ python_dictionary = {"string":"arrays of bytes representing unicode characters" 
 "Boolean operator":"used to represent the value of an expression" , 
 "if statement":"it checks for a given condition, if the condition is true, then the set of code present inside will be executed" , 
 "else statement":"An else statement contains the block of code that executes if the conditional expression in the if statement resolves to 0 or a FALSE value." ,
-"Dictionary":"used to store data values in key:value pairs" , 
-"Float":"A numerical value with a decimal component" , 
+"Dictionary":"used to store data values in key:value pairs" } 
+python_dictionary.update({"Float":"A numerical value with a decimal component" , 
 "Loop":"work through a collection of items, one at a time" , 
 "List":"A collection of items in particular order." , 
 "Key":"The first item in a key value pair in a dictionary." , 
-"Comment":"A note in a program that the python interpreter ignores"}
+"Comment":"A note in a program that the python interpreter ignores"})
 
 for i in python_dictionary:
     print(i ,":", "\n" + python_dictionary[i])

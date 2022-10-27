27125a9a5fbcc712bad11b7ee1a1bec59ff4614e
Afra Zareen <afrazareen04@gmail.com>
2022-10-27 16:24:46 +0400

    Update Exercise3.py

--- Chapter 7- Functions/Exercises/Exercise3.py
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -1,6 +1,6 @@
-def make_shirts(size,message):
-    """Summarize the shirt that is going to be made."""
-    print(f"\nI am going to make a {size} shirt ")
+def make_Tshirts(size,message):
+    """Summarize the Tshirt that is going to be made."""
+    print(f"\nI am going to make a {size} Tshirt ")
     print(f"The message is, '{message}'.")
-make_shirts("medium" , "Love Yourself")
-make_shirts("large" , "Believe in yourself")
+make_Tshirts("medium" , "Love Yourself")
+make_Tshirts("large" , "Believe in yourself")

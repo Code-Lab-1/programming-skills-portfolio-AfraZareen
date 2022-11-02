3a6c5a6386aa066ef41f219be75e23ccfaf0439b
Afra Zareen <afrazareen04@gmail.com>
2022-11-02 13:34:17 +0400

    Create Function1.py

---
+++ Chapter 7- Functions/Exercises/Function1.py
@@ -0,0 +1,17 @@
+def name():
+    print("Afra Zareen")
+name()
+def name(Fname,last):
+    print(Fname + last)
+name("Afra" , "Zareen")
+def sum(x,y):
+    print(x+y)
+sum(1,2)
+def sum(a,b,c,d):
+    print(a+b+c+d)
+sum(1,2,3,4)
+def fruit(food):
+    for x in food:
+        print(x)
+fruits = ["Apple" , "Banana" , "Mango"]
+fruit(fruits)

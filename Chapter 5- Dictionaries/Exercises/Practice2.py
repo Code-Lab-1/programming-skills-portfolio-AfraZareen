ec2606af7f88a46304e4ef1abfb6e2368557ee13
Afra Zareen <afrazareen04@gmail.com>
2022-11-09 11:11:13 +0400

    Create Practice2.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice2.py
@@ -0,0 +1,3 @@
+student = {"Name":"Afra" , "Age":"18" , "Class":"Year1" , "Gender":"Female" , "Location":"UAE" , "Email":"afrazareen04@gmail.com"}
+student.update({"Blood Group":"B+"})
+print(student)

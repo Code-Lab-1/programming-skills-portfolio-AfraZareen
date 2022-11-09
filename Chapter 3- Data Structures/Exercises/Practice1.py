9cdcfb5c513b8edf3e713ad634fa04d7363dde7b
Afra Zareen <afrazareen04@gmail.com>
2022-11-09 10:16:10 +0400

    Update Practice1.py

--- Chapter 3- Data Structures/Exercises/Practice1.py
+++ Chapter 3- Data Structures/Exercises/Practice1.py
@@ -6,5 +6,9 @@
 #Boolean Type:	bool
 #Binary Types:	bytes, bytearray, memoryview
 #None Type:	NoneType
+#List is a collection which is ordered and changeable. Allows duplicate members.
+#Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+#Set is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+#Dictionary is a collection which is ordered** and changeable. No duplicate members.
 this_list = ["John" , "Evelyn" , "Chris" ]
 print(len(this_list))

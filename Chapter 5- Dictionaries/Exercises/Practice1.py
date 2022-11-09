cb66b4df8e31bb28a0ef6833e3ba1b2035e1802e
Afra Zareen <afrazareen04@gmail.com>
2022-11-09 11:11:09 +0400

    Create Practice1.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice1.py
@@ -0,0 +1,4 @@
+student = {"Name":"Afra" , "Age":"18" , "Class":"Year1" , "Gender":"Female" , "Location":"UAE" , "Email":"afrazareen04@gmail.com"}
+print(student)
+print(student["Age"])
+print(student.keys())

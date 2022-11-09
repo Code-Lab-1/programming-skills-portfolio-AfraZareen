0cc294ba81c8dfd49e650022b065553ea8d8ef6d
Afra Zareen <afrazareen04@gmail.com>
2022-11-09 11:11:16 +0400

    Create Practice3.py

---
+++ Chapter 5- Dictionaries/Exercises/Practice3.py
@@ -0,0 +1,5 @@
+student = {"Name":"Afra" , "Age":"18" , "Class":"Year1" , "Gender":"Female" , "Location":"UAE" , "Email":"afrazareen04@gmail.com"}
+if "Name" in student:
+  print("Name is present")
+else:
+  print("Name is not in the dictionary")

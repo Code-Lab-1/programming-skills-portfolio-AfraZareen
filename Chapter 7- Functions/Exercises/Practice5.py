5f683ec2c8ad55b4ff352d4cad4181bddec26299
Afra Zareen <afrazareen04@gmail.com>
2022-11-10 16:24:50 +0400

    Create Practice5.py

---
+++ Chapter 7- Functions/Exercises/Practice5.py
@@ -0,0 +1,6 @@
+def greet(*names):
+    """This function greets all the person in the name tuple."""
+    for name in names:
+        print("Hello" , name)
+greet("Chris" , "John" , "James" , "Evelyn")
+    

afabe3097f420cb6bcc687f42abe7a03cf45752b
Afra Zareen <afrazareen04@gmail.com>
2022-11-11 22:55:17 +0400

    Update Practice3.py

--- Chapter 5- Dictionaries/Exercises/Practice3.py
+++ Chapter 5- Dictionaries/Exercises/Practice3.py
@@ -1,5 +1,7 @@
-student = {"Name":"Afra" , "Age":"18" , "Class":"Year1" , "Gender":"Female" , "Location":"UAE" , "Email":"afrazareen04@gmail.com"}
+student = {"Name":"Afra" , "Age":"18" , "Class":"Year1" , "Gender":"Female" , "Location":"UAE" , 
+"Email":"afrazareen04@gmail.com"}
 if "Name" in student:
   print("Name is present")
 else:
   print("Name is not in the dictionary")
+  
